1fecd874adaf4c13ae2d5e281ce4337f4ffd0811
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-05-17 17:51:28 +0200

    added services

--- TaskManagment/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ TaskManagment/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df49e0151c20cb13c2a02f1c63e46c65a34dcafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f3b387a26862e9603e362a83e8e3bfb58f5d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TaskManagment/service/services/TaskService.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using infrastructure.models;
+using infrastructure.repositories;
+
+namespace service.services
+{
+    public class TaskService
+    {
+        private readonly TaskRepository _repository;
+
+        public TaskService(TaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<TaskModel> GetAllTasks()
+        {
+            try
+            {
+                return _repository.GetAllTasks();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Could not get all tasks due to this {ex}!");
+            }
+        }
+        public TaskModel CreateTask(string taskName, string taskDescription, bool taskStatus)
+        {
+            var taskToCreate = new TaskModel
+            {
+                DateCreated = DateTime.UtcNow,
+                TaskName = taskName,
+                TaskDescription = taskDescription,
+                TaskStatus = false
+            };
+
+            try
+            {
+                return _repository.CreateTask(taskToCreate);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Could not create task:  due to this error {ex}!");
+            }
+        }
+        public void DeleteTask(int id)
+        {
+            try
+            {
+                _repository.DeleteTask(id);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Could not delete task with id: {id} due to this exception {ex}!");
+            }
+        }
+    }
+}
