51c1f69d37728ab9202eedb2aa5cb0db1b0173dc
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-05-18 12:43:49 +0200

    Added test to the sln

--- TaskManagment/service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ TaskManagment/service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fecd874adaf4c13ae2d5e281ce4337f4ffd0811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61714ca0aaf8e72c633e521a8dc125abe0d3e85d")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
