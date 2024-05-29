b684ea8d14956ee545518c34a590c9ac00f0dc0f
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-05-29 16:47:13 +0200

    Fix

--- TaskManagment/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ TaskManagment/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5055fdebcb7cf6b3a4a50c4476d15690cc3fb0e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc137726bc5bb91326abaa0b39cb76bfaf17220")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
