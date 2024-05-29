449e142d16ccb2626275e1f69b4c45275de2955b
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-05-29 17:57:10 +0200

    Add perfomance tests

---
+++ TaskManagment/tests/PerformanceTest/load.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+
+export let options = {
+    stages: [
+        { duration: '30s', target: 100 }, 
+        { duration: '50s', target: 100 }, 
+        { duration: '30s', target: 0 }, 
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+export default function () {
+    http.get('http://213.199.36.5:5001/tasks');
+}
--- TaskManagment/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ TaskManagment/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5055fdebcb7cf6b3a4a50c4476d15690cc3fb0e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9566a973ac0138b64e3f2a8c5f4b6f929ce07163")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
